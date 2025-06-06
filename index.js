d9bcdd295fec2693e7fa1f4861e84b2d8900c74e
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-06-06 23:59:54 +0600

    Create Collections

--- index.js
+++ index.js
@@ -22,18 +22,19 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    const db = client.db("restaurantDB");
+
+    const foodsCollection = db.collection("foods");
+    const ordersCollection = db.collection("orders");
+
+    
+
+    console.log("Connected to restaurantDB and APIs are ready!");
+  } catch (err) {
+    console.error(err);
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
